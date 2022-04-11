29922a737665e7a5a90035313943fc868e992b5c
nehabht <ne.bhatti2@icloud.com>
2022-04-11 14:48:52 +0200

    add snack 3

---
+++ snack-3/assets/js/main.js
@@ -0,0 +1,31 @@
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+//Crea un nuovo array con la lista dei mammiferi.
+
+const animali = [
+    { 
+        nome: 'leone', 
+        famiglia: 'felidi', 
+        classe: 'mammiferi'
+    },
+    { 
+        nome: 'cane', 
+        famiglia: 'canidi', 
+        classe: 'mammiferi' },
+    { 
+        nome: 'gallina', 
+        famiglia: 'fasianidi', 
+        classe: 'uccelli' 
+    },
+    { 
+        nome: 'gallina', 
+        famiglia: 'fasianidi', 
+        classe: 'uccelli' 
+    },
+]
+
+
+let animaliMammiferi = animali.filter(animale => animale.classe === "mammiferi");
+
+console.log(animaliMammiferi)
+
